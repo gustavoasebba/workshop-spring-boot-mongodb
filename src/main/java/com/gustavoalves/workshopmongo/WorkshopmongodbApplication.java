ad8b39509940ec662b57a6655e706c72a5814715
gustavoasebba <gustavoasebba@yahoo.com.br>
2019-01-21 10:02:25 -0200

    Primeiro commit - projeto criado

---
+++ src/main/java/com/gustavoalves/workshopmongo/WorkshopmongodbApplication.java
@@ -0,0 +1,14 @@
+package com.gustavoalves.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmongodbApplication.class, args);
+	}
+
+}
+
